dbb06ba85079b58eb77de2c80a2b2de0f2bddb37
BibicaNight <benardo49@hotmail.com>
2024-05-16 22:10:21 -0300

    aula sobre props e styles

--- react-course/src/App.jsx
+++ react-course/src/App.jsx
@@ -1,31 +1,24 @@
+import "./App.css";
 
-import "./App.css"
-
-import Banner from "./components/Banner"
-
-import emu from "./assets/images/svg/emu-svgrepo-com.svg"
-import hotel from "./assets/images/svg/hotel-svgrepo-com.svg"
-import map from "./assets/images/svg/map-svgrepo-com.svg"
-import polaroid from "./assets/images/svg/polaroid-svgrepo-com.svg"
-import suv from "./assets/images/svg/suv-svgrepo-com.svg"
-import bath from "./assets/images/svg/take-a-bath-svgrepo-com.svg"
+import Layout from "./components/Layout/index";
+import Pikachu from "./assets/images/svg/Pikachu.jpg"
 
 function App() {
   return (
-  <body>
-     <div className="card">
-     <h1>React Course</h1>
-     <p>Front-End Javascript</p>
-    </div>
-
-    <Banner titule="Transporte" description="use nosso transporte" image={emu} />
-    <Banner titule="Hotel" description="Se hospede no nosso hotel" image={hotel} />
-    <Banner titule="Mapa" description="Pegue o guia turistico" image={map} />
-    <Banner titule="Fotos" description="Fotografre em polaroid" image={polaroid} />
-    <Banner titule="Mobilidade" description="Use nosso carro" image={suv} />
-    <Banner titule="Banho" description="Use nossas incriveis banheiras" image={bath} />
-    
-  </body>
+    <body>
+      <div className="card">
+        <h1>React Course</h1>
+        <p>Front-End Javascript</p>
+      </div>
+      <Layout
+        tag="React"
+        title="Curso React - Senai"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat."
+        buttonLeft="Leia mais"
+        buttonRight="Documentação"
+        image={Pikachu}
+      />
+    </body>
   );
 }
 
