bfe5f159283878cb3933c884185deb3b48f0913a
BibicaNight <benardo49@hotmail.com>
2024-05-23 20:51:30 -0300

    feat: Pricing composition done

--- src/components/Pricing/src/pattern/composition/price-content.composition.jsx
+++ src/components/Pricing/src/pattern/composition/price-content.composition.jsx
@@ -1,25 +1,34 @@
 import '../../../styles.css'
 import { Check } from '../../assets/images';
 
+const benefits = [
+  1 ,2 ,3 ,4 ,5
+]
+
+function PriceBenefits(){
+  return (
+    <div className="price-benefits">
+      <img src={Check} alt="Check" />
+      <p>Feature text goes here</p>
+    </div> 
+  );
+}
+
 export default function PriceContent() {
   return (
-    <div>
-      <div className="box-border">
-      <div className="price-content">
+      <div className="box-container">
+        <div className="price-content">
         <h1>Basic plan</h1>
         <h2>$19/mo</h2>
         <p>or $199 yearly</p>
-        <div className="price-feature">
-          <p>Feature text goes here</p>
-          <img src={Check} alt="Check" />
-          <div>
-      <div className="button-start">
+        {benefits.map((benef) =>{
+
+        return <PriceBenefits key={benef}/>
+        })}
+          <div className="button-start">
               <p>Get started</p>
-            </div>
-          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,10 +1,19 @@
 import "./styles.css"
 import { Team } from "./src/pattern/composition"
 
+const mock = [
+    1, 2, 3, 4, 5, 6
+]
 export default function TeamComposition() {
     return (
         <div className="team-container">
             <Team.Section />
+
+        {mock.map((element) =>{
+
+            return <Team.Content key={element}/>
+        })}
+        
             <Team.Content />
             <Team.Main />
         </div>
