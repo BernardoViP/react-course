4b26e26db503985875b193725305107cb6795cd9
BibicaNight <benardo49@hotmail.com>
2024-05-23 22:04:06 -0300

    dynamic and mapping team

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import "./App.css"
-import PriceContent from "./components/Pricing/src/pattern/composition/price-content.composition";
-import PriceSection from "./components/Pricing/src/pattern/composition/price-section.composition";
-
-
 
 function App() {
   return (
     <body>
-      <PriceSection/>
-      <PriceContent/>
+      
     </body>
   );
 }
--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,21 +1,45 @@
 import "./styles.css"
 import { Team } from "./src/pattern/composition"
 
-const mock = [
-    1, 2, 3, 4, 5, 6
+const TeamUsers = [
+    {
+        image:"https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/114.png",
+        name:"Bernardo Vieira",
+        role:"Estudante",
+        description:"Aprendiz do trigrinho",
+            socialMedia:{
+                Linkedin:"https://br.linkedin.com/",
+                X:"https://x.com/?lang=pt-br",
+                Dribble:"https://dribbble.com/"  
+            }
+    },
 ]
+
 export default function TeamComposition() {
-    return (
-        <div className="team-container">
-            <Team.Section />
-
-        {mock.map((element) =>{
-
-            return <Team.Content key={element}/>
-        })}
+  return (
+    <div className="team-container">
+      <Team.Section />
+      <div className="team-content-wrapper">
         
-            <Team.Content />
-            <Team.Main />
-        </div>
-    )
+        {
+            TeamUsers.map((user, key) => {
+            return(
+                <Team.Content
+                key= {key}
+                image={user.image}
+                name={user.name}
+                role={user.role}
+                description={user.description}
+                Linkedin={user.socialMedia.Linkedin}
+                X={user.socialMedia.X}
+                Dribble={user.socialMedia.Dribble}
+            />
+            )
+})}
+
+
+      </div>
+      <Team.Main />
+    </div>
+  );
 }
