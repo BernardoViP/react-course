19a29277feef19ae8eb4fab8e902735a7ff2d764
BibicaNight <benardo49@hotmail.com>
2024-05-23 21:11:29 -0300

    Feat: price composition props

--- src/components/Pricing/src/pattern/composition/price-content.composition.jsx
+++ src/components/Pricing/src/pattern/composition/price-content.composition.jsx
@@ -5,28 +5,28 @@ const benefits = [
   1 ,2 ,3 ,4 ,5
 ]
 
-function PriceBenefits(){
+function PriceBenefits(props){
   return (
     <div className="price-benefits">
-      <img src={Check} alt="Check" />
-      <p>Feature text goes here</p>
+      <img src={props.image ?  props.image : Check} alt="Check" />
+      <p>{props.text}</p>
     </div> 
   );
 }
 
-export default function PriceContent() {
+export default function PriceContent(props) {
   return (
       <div className="box-container">
         <div className="price-content">
-        <h1>Basic plan</h1>
-        <h2>$19/mo</h2>
-        <p>or $199 yearly</p>
+        <h1>{props.basic}</h1>
+        <h2>{props.month}</h2>
+        <p>{props.yearly}</p>
         {benefits.map((benef) =>{
 
         return <PriceBenefits key={benef}/>
         })}
           <div className="button-start">
-              <p>Get started</p>
+              <p>{props.button}</p>
         </div>
       </div>
     </div>
