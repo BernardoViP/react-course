61ecb99e0665fc2d31ae09f5bc73b68869654f0e
BibicaNight <benardo49@hotmail.com>
2024-05-28 21:43:07 -0300

    Styled Components Dinamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css"
+import Square from "./styles/styled-components/components/Square";
 
 function App() {
   return (
     <body>
-      
+      <Square color="red" borderStyle="dotted" />
     </body>
   );
 }
---
+++ src/styles/styled-components/components/Square/style.js
@@ -0,0 +1,19 @@
+
+
+import styled from "styled-components";
+
+export const SquareContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px ${props => props.borderStyle ? props.borderStyle : "solid"} black;
+    width: 200px;
+    height: 200px;
+    background-color: ${props => props.color ? props.color : "white"};
+`
+
+export const SquareLink = styled.a`
+    text-decoration: none;
+    font-size: 18px;
+    color: black;
+`
