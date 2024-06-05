9592d72e275c0ffc7fbc0f7be356867bf46b5395
BibicaNight <benardo49@hotmail.com>
2024-06-04 21:51:20 -0300

    modules css

--- src/styles/header-module/components/Header/index.jsx
+++ src/styles/header-module/components/Header/index.jsx
@@ -6,7 +6,6 @@ export default function Header(){
         <p className={styles.tag}>Tagline</p>
         <h1 className={styles.head}>Short heading here</h1>
         <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, culpa. Sapiente quia sit illo vitae est doloribus alias impedit amet perspiciatis recusandae eveniet libero maiores, rerum quam, iure vel sed!</p>
-        <div className={styles.buttom}>Button</div>
         </div>
     )
 }
