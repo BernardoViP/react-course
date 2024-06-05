e1a3e8b306b551353886191a42d115f92362fb04
BibicaNight <benardo49@hotmail.com>
2024-06-04 22:12:59 -0300

    modules css creating

--- src/styles/header-module/components/Header/index.jsx
+++ src/styles/header-module/components/Header/index.jsx
@@ -1,11 +1,15 @@
 import styles from "./style.module.css"
+import Image from "../../assets/image/Image.png"
 
 export default function Header(){
     return(
         <div className={styles.back}>
-        <p className={styles.tag}>Tagline</p>
+        <p className={styles.tag}>Blog</p>
         <h1 className={styles.head}>Short heading here</h1>
-        <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, culpa. Sapiente quia sit illo vitae est doloribus alias impedit amet perspiciatis recusandae eveniet libero maiores, rerum quam, iure vel sed!</p>
+        <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+        <div className={styles.boximg}>
+            <img src={Image} alt="" />
         </div>
-    )
+        </div>
+)
 }
