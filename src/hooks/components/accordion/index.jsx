467b43ec729e7a362486419089e87ff5caec9b95
BibicaNight <benardo49@hotmail.com>
2024-06-05 21:41:37 -0300

    Accordion and useState hook component

--- src/hooks/components/accordion/index.jsx
+++ src/hooks/components/accordion/index.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import { Down, Up } from "./src/svg"
-import { AccordionButton, AccordionContainer, AccordionImage, AccordionText } from "./style"
+import { AccordionButton, AccordionContainer, AccordionContent, AccordionImage, AccordionText, AccordionWrapper, ContentText } from "./style"
 
 
 export default function Accordion() {
+
+    function HandleAccordion (){
+        return setVisible(!visible)
+    }
+//  controle de estado do content
+    const [ visible, setVisible ] = useState(false)
+// validação da direção da seta
+    const handleAngle = visible === false ? Down : Up
+
     return(
+    <AccordionWrapper>
      <AccordionContainer>
         <AccordionText>What is an accordion component?</AccordionText>
-        <AccordionButton>
-            <AccordionImage src={Up}/>
+        <AccordionButton onClick={HandleAccordion}>
+            <AccordionImage src={handleAngle}/>
         </AccordionButton>
      </AccordionContainer>
-    )
+
+     {visible && (
+        <AccordionContent>
+            <ContentText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque porro ex error dolores excepturi. Qui eum ea, magnam odio rem vero eveniet quaerat ab nisi sed ipsam ipsum reiciendis modi.</ContentText>
+        </AccordionContent>
+     )}
+     </AccordionWrapper>
+    );
 }
